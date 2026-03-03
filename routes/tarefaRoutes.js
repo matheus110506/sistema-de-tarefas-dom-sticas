22985d6e7db0cd15bc11e52b6a3e76d33bf2d9f1
matheus110506 <matheus110506@hotmail.com>
2026-03-03 20:40:32 -0300

    Add delete route for tarefas

--- routes/tarefaRoutes.js
+++ routes/tarefaRoutes.js
@@ -6,5 +6,6 @@ router.post('/tarefas', tarefaController.criarTarefa);
 router.get('/tarefas/mae/:maeId', tarefaController.listarTarefasPorMae);
 router.get('/tarefas/filho/:filhoId', tarefaController.listarTarefasPorFilho);
 router.put('/tarefas/concluir', tarefaController.marcarConcluida);
+router.delete('/tarefas/:id', tarefaController.excluirTarefa);
 
 module.exports = router;

88181f638ea7159018cda4669d1488686f47537f
matheus110506 <matheus110506@hotmail.com>
2026-03-06 11:19:12 -0300

    Add files via upload

---
+++ controllers/adminController.js
@@ -0,0 +1,54 @@
+const db = require('../db');
+
+exports.loginAdmin = async (req, res) => {
+
+    const { email, senha } = req.body;
+
+    if (email === "admin@gmail.com" && senha === "sAs45678") {
+        return res.json({
+            nome: "Admin",
+            tipo: "admin"
+        });
+    }
+
+    return res.status(401).json({
+        erro: "Login inválido"
+    });
+};
+
+
+exports.listarLogs = async (req, res) => {
+
+    try {
+
+        const [logs] = await db.query(
+            "SELECT * FROM logs ORDER BY data DESC"
+        );
+
+        res.json(logs);
+
+    } catch (erro) {
+        res.status(500).json({ erro: "Erro ao buscar logs" });
+    }
+
+};
+
+
+exports.banirIP = async (req, res) => {
+
+    try {
+
+        const { ip } = req.body;
+
+        await db.query(
+            "INSERT INTO ips_banidos (ip) VALUES (?)",
+            [ip]
+        );
+
+        res.json({ message: "IP banido" });
+
+    } catch (erro) {
+        res.status(500).json({ erro: "Erro ao banir IP" });
+    }
+
+};
